f1d4a5f2f57c837c60b2b92b4e859824da4d3854
David Balatero <dbalatero@gmail.com>
2010-11-04 12:41:46 -0700

    Fixed ffi_lib loading on Ubuntu/Linux.

--- lib/levenshtein.rb
+++ lib/levenshtein.rb
@@ -3,12 +3,10 @@ require 'ffi'
 module Levenshtein
   extend FFI::Library
 
+  # Try loading in order.
   library = File.dirname(__FILE__) + "/../ext/levenshtein/levenshtein"
-  begin
-    ffi_lib(library)
-  rescue LoadError
-    ffi_lib(library + ".bundle")
-  end
-
+  candidates = ['.bundle', '.so', '.dylib', ''].map { |ext| library + ext }
+  ffi_lib(candidates)
+  
   attach_function :distance, :levenshtein, [:string, :string], :int
 end
